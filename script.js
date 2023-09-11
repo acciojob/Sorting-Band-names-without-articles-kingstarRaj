97b52ddfe4958c33abf0fde5ef469118c5c67a9e
RAJA SAHU <117908592+kingstarRaj@users.noreply.github.com>
2023-09-11 17:58:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-
-// suppose an array that contains a list of popular tourise spots
+// Suppose an array that contains a list of popular tourist spots
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
 let arr = [];
 let mapp = {};
 
-for(let i = 0; i<tousristSpots.length; i++){
-	let word = touristSpots.split(" ");
-	let finalstr = "";
+for (let i = 0; i < touristSpots.length; i++) {
+  let words = touristSpots[i].split(" "); // Split the current spot into words
+  let finalstr = "";
 
-	//iterating over the word array
-	for(let i = 0 ; i<word.length; i++){
-		let currentword = word[i].toLowerCase();
+  // Iterating over the words array
+  for (let j = 0; j < words.length; j++) {
+    let currentWord = words[j].toLowerCase();
 
-		//addinng word without article to final string
-		if(currentword !== 'an' || currentword !== 'the' || currentword !== 'a'){
-			finalstr += currentword + " ";
-		}
-	}
+    // Adding words without articles to final string
+    if (currentWord !== 'an' && currentWord !== 'the' && currentWord !== 'a') {
+      finalstr += currentWord + " ";
+    }
+  }
 
-	let artclelessstr = finalstr.trim();
-	mapp[artclelessstr] = touristSpots[i];
-	arr.push(artclelessstr);
+  let articlelessStr = finalstr.trim();
+  mapp[articlelessStr] = touristSpots[i];
+  arr.push(articlelessStr);
 }
 
 arr.sort();
 
-for(let i = 0; i<arr.length; i++){
-	touristSpots[i] = mapp[arr[i]]; 
+for (let i = 0; i < arr.length; i++) {
+  touristSpots[i] = mapp[arr[i]];
 }
 
 console.log(touristSpots);
